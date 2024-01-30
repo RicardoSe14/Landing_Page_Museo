e89bde8c8a3a1c0930c14d1896752c5ced44ade9
RicardoSe14 <Ricardo.s14@gmail.com>
2024-01-30 02:10:06 -0300

    Completo

---
+++ assets/js/museo.js
@@ -0,0 +1,65 @@
+$("#barra").click(function(){
+    $(".esconder").slideToggle(100);
+  });
+
+
+  $(".boton1").click(function(){
+    $(".a2024").show(0);
+  });
+
+  $(".boton1").click(function(){
+    $(".a2019, .a2014, .a2009, .a2004").hide(0);
+  });
+
+  $(".boton2").click(function(){
+    $(".a2019").show(0);
+  });
+
+  $(".boton2").click(function(){
+    $(".a2024, .a2014, .a2009, .a2004").hide(0);
+  });
+
+  $(".boton3").click(function(){
+    $(".a2014").show(0);
+  });
+
+  $(".boton3").click(function(){
+    $(".a2024, .a2019, .a2009, .a2004").hide(0);
+  });
+
+  $(".boton4").click(function(){
+    $(".a2009").show(0);
+  });
+
+  $(".boton4").click(function(){
+    $(".a2024, .a2019, .a2014, .a2004").hide(0);
+  });
+
+  $(".boton5").click(function(){
+    $(".a2004").show(0);
+  });
+
+  $(".boton5").click(function(){
+    $(".a2024, .a2019, .a2009, .a2014").hide(0);
+  });
+
+
+  $(".boton0").click(function(){
+    $(".a2024, .a2019, .a2009, .a2004, .a2014").show(0);
+  });
+
+  $(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+  
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
